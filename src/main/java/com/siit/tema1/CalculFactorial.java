3424e32aa914b8e41f20efd802a408f5a54f6ad2
robert979 <robert.serbanoiu@yahoo.com>
2020-02-25 10:33:34 +0200

    prima tema

---
+++ src/main/java/com/siit/tema1/CalculFactorial.java
@@ -0,0 +1,22 @@
+package com.siit.tema1;
+
+import java.util.Scanner;
+
+public class CalculFactorial {
+    public static void main(String... args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti numarul: ");
+        int i = scanner.nextInt();
+        //System.out.println(i);
+
+
+        int c = 1;
+        for (int a = 1; a <=i; a++) {
+            c = c * a;
+
+        }
+        System.out.println(i+"! = " + c);
+    }
+
+}
