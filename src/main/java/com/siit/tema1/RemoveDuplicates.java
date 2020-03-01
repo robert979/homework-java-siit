29f4795614698032de690ededcbf6c0003fea28e
robert979 <robert.serbanoiu@yahoo.com>
2020-03-01 12:20:04 +0200

    done

--- src/main/java/com/siit/tema1/RemoveDuplicates.java
+++ src/main/java/com/siit/tema1/RemoveDuplicates.java
@@ -1,41 +1,44 @@
 package com.siit.tema1;
 
 
-
-
-
-
-//import com.sun.tools.javac.util.ArrayUtils;
-
 import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
 
-        int[] sir = {20, 20, 30, 40, 50, 50, 50};
+        int[] sir = {20, 20, 30, 40, 50, 50, 50, 20};
+        Arrays.sort(sir);
+        System.out.println(Arrays.toString(sir));
+        int lungime = sir.length;
+        int b = lungime;
+        System.out.println("lungime = " + lungime);
+        if (lungime == 0 || lungime == 1) {
+            System.out.println(Arrays.toString(sir));
+        }
+        for (int i = 0; i <= lungime - 2; i++) {
+            if (sir[i] == sir[i + 1]) {
+                b = b - 1;
+            }
 
-        int a = sir.length;
-        int b = 0;
 
-        //System.out.println("Arata sirul initial" + Arrays.toString(sir));
+        }
+        System.out.println("lungimea sirului fara duble este  " + b);
+        int[] faraduble = new int[b];
 
+        int k = 0;
+        for (int i = 0; i <= lungime - 2; i++) {
+            if (sir[i] != sir[i + 1]) {
+                faraduble[k] = sir[i];
+                k++;
+                faraduble[b-1] = sir[lungime-1];
 
-        for (int i = 0; i <= a - 2; i++) {
-
-            for (int j = i+1; j <= i+1; j++) {
-                if (sir[i] != sir[j]) {
-                    b = b + 1;
-                    System.out.println("b = " + b);
-                    // faraduble [i] = sir[i];}
-                    //System.out.println("Arata sirul intermediar" + Arrays.toString(faraduble));
-                    //else {
-
-                }
-                int []faraduble= new int[b];
 
             }
-            // System.out.println("Arata sirul" + Arrays.toString(faraduble));
+
         }
+        System.out.println("Array fara duble este " +Arrays.toString(faraduble));
+
+
     }
 }
 
@@ -44,27 +47,6 @@ public class RemoveDuplicates {
 
 
 
-
-        /*int b=a;
-        int [] faraduplicate=new int[b];
-        for(int i=0; i<a-2; i++){
-            for (int j =i+1;j<a-1 ; j++ ){
-                if (sir[i]==sir[j]) {
-                    b=b-1;
-
-                    int [] sirfaraduplicate=new int[a-1] ;
-
-                }
-                else if (sir[i]!=sir[j]) {
-
-
-                }
-
-            }
-            for(int k =0; k<a; k++){
-                System.out.println("sir[k] = " + sir[k]);
-            }
-            */
         
 
 
