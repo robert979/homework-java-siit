806a0b91dd9c7245814389ea0737b421490b356a
robert979 <robert.serbanoiu@yahoo.com>
2020-03-03 16:45:19 +0200

    done

---
+++ src/main/java/com/siit/tema2/FactorialMetode.java
@@ -0,0 +1,24 @@
+package com.siit.tema2;
+
+import java.util.Scanner;
+
+public class FactorialMetode {
+    static public void main(String [] args){
+        Scanner numar = new Scanner(System.in);
+        System.out.println("Introduceti numarul al carui factorial doriti sa6=-l calculati");
+        int n = numar.nextInt();
+        returnFactorial(n);
+
+    }
+    static public void returnFactorial(int n){
+        int c=1;
+        for (int i=1; i<=n; i++) {
+             c = c * i;
+
+        }
+
+        System.out.println(n + "! = " + c);
+
+    }
+
+}
