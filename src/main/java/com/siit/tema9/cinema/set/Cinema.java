1d432ffbd3ec5aa5fcb9fd6849a78b017b946a78
Robert_PC <robert.serbanoiu@yahoo.com>
2020-05-06 17:20:57 +0300

    Collections

--- src/main/java/com/siit/tema9/cinema/set/Cinema.java
+++ src/main/java/com/siit/tema9/cinema/set/Cinema.java
@@ -1,13 +1,65 @@
 package com.siit.tema9.cinema.set;
 
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
+
+@ToString
 
 public class Cinema {
     private String cinemaName;
     private Set<Seat> seat;
+    private BigDecimal ballance;
 
-    public Cinema(String cinemaName, Set<Seat> seat) {
+    public Cinema(String cinemaName, Set<Seat> seat, BigDecimal ballance) {
         this.cinemaName = cinemaName;
+        this.seat = seat;
+        this.ballance = ballance;
+    }
+
+    public static void checkAllAvailableseats(Set<Seat> seats) {
+        System.out.println("The following seats are available");
+        for (Character ch = 'A'; ch <= 'J'; ch++) {
+            for (int i = 1; i <= 10; i++) {
+                if (seats.contains(new Seat(ch.toString(), i))) {
+                    System.out.printf(" " + ch + "%02d", i);
+                }
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public static Set<Seat> createCinemaSeats(Character ch) {
+        Set<Seat> seatsCinema = new HashSet<>();
+        for (Character row = 'A'; row <= ch; row++) {
+            for (int i = 1; i <= 10; i++) {
+                Seat seat = new Seat(ch.toString(), 10);
+                seatsCinema.add(seat);
+            }
+        }
+
+        return seatsCinema;
+
+    }
+
+    public String getCinemaName() {
+        return cinemaName;
+    }
+
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+
+    public Set<Seat> getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Set<Seat> seat) {
         this.seat = seat;
     }
 
@@ -24,13 +76,23 @@ public class Cinema {
         }
     }
 
-    public static boolean reserveSeat(Set<Seat> seats, Set<Seat> reservedSeats, String row, int seatNr) {
+    public BigDecimal getBallance() {
+        return ballance;
+    }
+
+    public void setBallance(BigDecimal ballance) {
+        this.ballance = ballance;
+    }
+
+    public boolean reserveSeat(Set<Seat> seats, Set<Seat> reservedSeats, String row, int seatNr) {
         if (seats.contains(new Seat(row, seatNr))) {
             System.out.printf("Your seat " + row + "%02d", seatNr);
             System.out.print(" was reserved");
+            this.ballance = this.ballance.add(BigDecimal.valueOf(10.50));
 
             seats.remove(new Seat(row, seatNr));
             reservedSeats.add(new Seat(row, seatNr));
+
             return true;
 
         } else {
@@ -39,33 +101,22 @@ public class Cinema {
         }
     }
 
-    public static boolean cancelReservation(Set<Seat> availableSeats, Set<Seat> reservedSeats, String row, int seatNr) {
+    public boolean cancelReservation(Set<Seat> availableSeats, Set<Seat> reservedSeats, String row, int seatNr) {
         if (reservedSeats.contains(new Seat(row, seatNr))) {
             System.out.println();
             System.out.printf("Your reservation for the seat " + row + "%02d", seatNr);
-            System.out.println(" was canceled ");
+            System.out.print(" was canceled ");
+            System.out.println();
+            System.out.println("And 10.50 € were returned");
             reservedSeats.remove(new Seat(row, seatNr));
             availableSeats.add(new Seat(row, seatNr));
+            this.ballance = this.ballance.subtract(BigDecimal.valueOf(10.50));
             return true;
         } else {
-            System.out.println("Your cancel request was rejected - the seat doesn't not appear in our data base as reserved");
+            System.out.println("Your cancel request was rejected - the seat doesn't not appear in our data base as reserved\n");
+            System.out.println();
             return false;
         }
     }
-
-    public static void checkAllAvailableseats(Set<Seat> seats) {
-        System.out.println("The following are available");
-        for (Character ch = 'A'; ch <= 'J'; ch++) {
-            for (int i = 1; i <= 10; i++) {
-                if (seats.contains(new Seat(ch.toString(), i))) {
-                    System.out.printf(" " + ch + "%02d", i);
-                }
-
-            }
-            System.out.println();
-        }
-
-    }
-
 
 }
--- src/main/java/com/siit/tema9/cinema/set/CinemaReservationService.java
+++ src/main/java/com/siit/tema9/cinema/set/CinemaReservationService.java
@@ -1,5 +1,6 @@
 package com.siit.tema9.cinema.set;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -10,6 +11,9 @@ public class CinemaReservationService {
         Scanner input = new Scanner(System.in);
         Set<Seat> seats = new HashSet<>();
         Set<Seat> reservedSeats = new HashSet<>();
+        String intialStr = "yes";
+        String allRows = "A©B©C©D©E©F©G©H©I©J ";
+        String allseatsNumbers = "1©2©3©4©5©6©7©8©9©10";
 
         for (Character letters = 'A'; letters <= 'J'; letters++) {
             for (int i = 1; i <= 10; i++) {
@@ -18,14 +22,89 @@ public class CinemaReservationService {
                 seats.add(seatTemporar);
             }
 
-            Cinema cinema = new Cinema("Gloria", seats);
         }
-        System.out.println("Please insert the name of your Cinema");
-        String cinemaName = input.nextLine();
-        System.out.println("How many rows do you want your Cinema to have ? \n" +
-                "(please chose any leeter from A to Z)");
-        String maxRow = input.nextLine().toUpperCase();
+        String cinemaName = "Scala";
+        Cinema cinema = new Cinema(cinemaName, seats, BigDecimal.valueOf(0.00));
 
+
+        while (intialStr.equals("yes")) {
+            System.out.println("Please make choice:\n" +
+                    "1) check all available seats.\n" +
+                    "2) reserve a seat.\n" +
+                    "3) cancel a reservation\n" +
+                    "4) check ballance");
+            String str = input.nextLine();
+            if (str.equals("1")) {
+                Cinema.checkAllAvailableseats(cinema.getSeat());
+                System.out.println("Do you want to go back to the meniu, please type \"yes\" if you do.");
+                System.out.println();
+                intialStr = input.nextLine().toLowerCase();
+            } else if (str.equals("2")) {
+                System.out.println("The price for one ticket is 10.50 €." +
+                        "" +
+                        "\nIn order to reserve a seat please insert the row from \"A\" to \"J\"");
+                String row = input.nextLine().toUpperCase();
+                if (allRows.contains(row)) {
+                    System.out.println("please insert the seat number you want to reserve");
+                    String seatNumber = input.nextLine();
+                    if (allseatsNumbers.contains(seatNumber)) {
+                        cinema.reserveSeat(seats, reservedSeats, row, Integer.parseInt(seatNumber));
+                        System.out.println();
+                        System.out.println("Do you want to go back to the meniu, please type \"yes\" if you do.");
+                        intialStr = input.nextLine().toLowerCase();
+                    } else {
+                        System.out.println("Wrong seat number.\n" +
+                                "Do you want to go back to the meniu, please type \"yes\" if you do.");
+                        intialStr = input.nextLine().toLowerCase();
+                    }
+                } else {
+                    System.out.println("This row doesn't exists.\n" +
+                            "Do you want to go back to the meniu, please type \"yes\" if you do.");
+                    intialStr = input.nextLine().toLowerCase();
+
+                }
+
+            } else if (str.equals("4")) {
+                System.out.println();
+                System.out.println("Cinema\'s ballance is " + cinema.getBallance() + " €\n" +
+                        "This means that there are " + cinema.getBallance().divide(BigDecimal.valueOf(10.5)) + " tickets sould\n" +
+                        "And there are stil " + BigDecimal.valueOf(100).subtract(cinema.getBallance().divide(BigDecimal.valueOf(10.5))) + " tickets available");
+                System.out.println();
+                System.out.println("Do you want to go back to the meniu, please type \"yes\" if you do.");
+                intialStr = input.nextLine().toLowerCase();
+            } else if (str.equals("3")) {
+                System.out.println("What is the seat that you want to cancel the reservation fo\n" +
+                        "Please type the row");
+                String row = input.nextLine().toUpperCase();
+                if (allRows.contains(row)) {
+                    System.out.println("Please type the seat number");
+                    {
+                        String seatNumber = input.nextLine();
+                        if (allseatsNumbers.contains(seatNumber)) {
+                            cinema.cancelReservation(seats, reservedSeats, row, Integer.parseInt(seatNumber));
+                        } else {
+                            System.out.println("Wrong seat number.\n" +
+                                    "Do you want to go back to the meniu, please type \"yes\" if you do.");
+                            intialStr = input.nextLine().toLowerCase();
+                        }
+                    }
+                } else {
+                    System.out.println("This row doesn't exists.\n" +
+                            "Do you want to go back to the meniu, please type \"yes\" if you do.");
+                    intialStr = input.nextLine().toLowerCase();
+
+                }
+            } else {
+                System.out.println("There is no such option as \"" + str + "\"");
+                System.out.println();
+                System.out.println("Do you want to go back to the meniu, please type \"yes\" if you do.");
+                intialStr = input.nextLine().toLowerCase();
+                System.out.println();
+                System.out.println();
+
+            }
+
+        }
 
     }
 
