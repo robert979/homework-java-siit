2ece923e75f1bf314ec7b648afb590844f00f4f7
Robert_PC <robert.serbanoiu@yahoo.com>
2020-05-06 21:27:50 +0300

    Collections

--- src/main/java/com/siit/tema9/appalegeri/AppAlegeri.java
+++ src/main/java/com/siit/tema9/appalegeri/AppAlegeri.java
@@ -35,23 +35,69 @@ public class AppAlegeri {
         BigDecimal Costache = BigDecimal.valueOf(0);
         BigDecimal Voicu = BigDecimal.valueOf(0);
         BigDecimal Protopopescu = BigDecimal.valueOf(0);
+        BigDecimal canceledVotes = BigDecimal.valueOf(0);
         String strAnswer = "yes";
         for (int i = 0; i <= allInformation.length() - 22; i++) {
             if (allInformation.substring(i, i + 3).equals("CNP")) {
-                String strTemp = allInformation.substring(i + 5, i + 19);
+                String strTemp = allInformation.substring(i + 5, i + 18);
                 cnpList.add(strTemp);
             }
+
         }
+        System.out.println(cnpList.contains("1800306040735"));
         for (String str : cnpListNonvoted) {
             System.out.println(str);
         }
         while (strAnswer.equals("yes")) {
-            System.out.println("Please chose your action");
-            System.out.println("Please insert your CNP");
-            String cnpReader = input.nextLine();
-            if (cnpList.contains(cnpReader)) {
-                if (!cnpListVoted.contains(cnpReader)) {
-                    System.out.println();
+            System.out.println("Please chose your action\n" +
+                    "1) Vote\n" +
+                    "2) Check intermediate pool\n" +
+                    "3) End vote.");
+            String actionStr = input.nextLine();
+            if (actionStr.equals("1")) {
+                System.out.println("Please insert your CNP");
+                String cnpReader = input.nextLine();
+                if (cnpList.contains(cnpReader)) {
+                    if (!cnpListVoted.contains(cnpReader)) {
+                        System.out.println();
+                        System.out.println("Please vote by typing the digit corresponding to the chosen candidate, from the list :\n" +
+                                "1) Ionescu\n" +
+                                "2) Popescu\n" +
+                                "3) Costache\n" +
+                                "4) Voicu\n" +
+                                "5) Protopoescu\n" +
+                                "6) Vote for no one");
+                        String choiceStr = input.nextLine();
+                        if (choiceStr.equals("1")) {
+                            cnpListVoted.add(cnpReader);
+                            cnpListNonvoted.remove(cnpReader);
+                            Ionescu = Ionescu.add(BigDecimal.valueOf(1));
+                        } else if (choiceStr.equals("2")) {
+                            cnpListVoted.add(cnpReader);
+                            cnpListNonvoted.remove(cnpReader);
+                            Popescu = Popescu.add(BigDecimal.valueOf(1));
+                        } else if (choiceStr.equals("3")) {
+                            cnpListVoted.add(cnpReader);
+                            cnpListNonvoted.remove(cnpReader);
+                            Costache.add(BigDecimal.valueOf(1));
+                        } else if (choiceStr.equals("4")) {
+                            cnpListVoted.add(cnpReader);
+                            cnpListNonvoted.remove(cnpReader);
+                            Voicu.add(BigDecimal.valueOf(1));
+                        } else if (choiceStr.equals("5")) {
+                            cnpListVoted.add(cnpReader);
+                            cnpListNonvoted.remove(cnpReader);
+                            Protopopescu.add(BigDecimal.valueOf(1));
+                        } else if (choiceStr.equals("6")) {
+                            cnpListVoted.add(cnpReader);
+                            cnpListNonvoted.remove(cnpReader);
+                            canceledVotes.add(BigDecimal.valueOf(0));
+                        } else {
+                            System.out.println("Your choice doen't match the option");
+                            System.out.println("Do you want to continue elections, please type \"yes\" if you do ");
+                            strAnswer = input.nextLine();
+                        }
+                    }
                 }
             }
 
