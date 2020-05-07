1d73cfdf91b4415353fa2e601b3987773eaed7ca
Robert_PC <robert.serbanoiu@yahoo.com>
2020-05-07 16:08:17 +0300

    Collections

--- src/main/java/com/siit/tema9/appalegeri/AppAlegeri.java
+++ src/main/java/com/siit/tema9/appalegeri/AppAlegeri.java
@@ -27,8 +27,8 @@ public class AppAlegeri {
         //System.out.println(allInformation);
         List<String> cnpList = new ArrayList<>();
         List<String> cnpListVoted = new ArrayList<>();
-        List<String> cnpListNonvoted = cnpList;
         int counterVote = 0;
+        int checkElectionFinish = 0;
         Comparator<Candidat> comparatorulMeu = new Comparator<Candidat>() {
             @Override
             public int compare(Candidat o1, Candidat o2) {
@@ -69,109 +69,177 @@ public class AppAlegeri {
             }
 
         }
-        System.out.println("The ist with all CNP is: ");
+        System.out.println("The list with all CNP is: ");
 
-        for (String str : cnpListNonvoted) {
+        for (String str : cnpList) {
             System.out.println(str);
         }
-        while (strAnswer.equals("yes") && counterVote != 10) {
-            System.out.println("Please chose your action:\n" +
-                    "1) Vote\n" +
-                    "2) Check intermediate pool\n" +
-                    "3) End vote.");
-            String actionStr = input.nextLine();
-            if (actionStr.equals("1")) {
-                System.out.println("Please insert your CNP");
-                String cnpReader = input.nextLine();
-                if (cnpList.contains(cnpReader)) {
-                    if (!cnpListVoted.contains(cnpReader)) {
-                        System.out.println();
-                        System.out.println("Please vote by typing the digit corresponding to the chosen candidate, from the list :\n" +
-                                "1) Ionescu\n" +
-                                "2) Popescu\n" +
-                                "3) Costache\n" +
-                                "4) Voicu\n" +
-                                "5) Vote for no one");
-                        String choiceStr = input.nextLine();
-                        if (choiceStr.equals("1")) {
-                            cnpListVoted.add(cnpReader);
-                            cnpListNonvoted.remove(cnpReader);
-                            ionescu.setVotes(ionescu.getVotes().add(BigDecimal.valueOf(1)));
-                            System.out.println(ionescu.getVotes());
-                            counterVote = counterVote + 1;
-                            System.out.println("So far " + counterVote + " people voted");
-                        } else if (choiceStr.equals("2")) {
-                            cnpListVoted.add(cnpReader);
-                            cnpListNonvoted.remove(cnpReader);
-                            popescu.setVotes(popescu.getVotes().add(BigDecimal.valueOf(1)));
-                            counterVote = counterVote + 1;
-                            System.out.println("So far " + counterVote + " people voted");
+        while (strAnswer.equals("yes")) {
+            if (counterVote != 10) {
+                System.out.println("Please chose your action:\n" +
+                        "1) Vote\n" +
+                        "2) Check intermediate pool\n" +
+                        "3) End vote.");
+                String actionStr = input.nextLine();
+                if (actionStr.equals("1")) {
+                    System.out.println("Please insert your CNP");
+                    String cnpReader = input.nextLine();
+                    if (cnpList.contains(cnpReader)) {
+                        if (!cnpListVoted.contains(cnpReader)) {
+                            System.out.println();
+                            System.out.println("Please vote by typing the digit corresponding to the chosen candidate, from the list :\n" +
+                                    "1) Ionescu\n" +
+                                    "2) Popescu\n" +
+                                    "3) Costache\n" +
+                                    "4) Voicu\n" +
+                                    "5) Vote for no one");
+                            String choiceStr = input.nextLine();
+                            if (choiceStr.equals("1")) {
+                                cnpListVoted.add(cnpReader);
+                                ionescu.setVotes(ionescu.getVotes().add(BigDecimal.valueOf(1)));
+                                System.out.println(ionescu.getVotes());
+                                counterVote = counterVote + 1;
+                                System.out.println("So far " + counterVote + " people voted");
+                            } else if (choiceStr.equals("2")) {
+                                cnpListVoted.add(cnpReader);
 
-                        } else if (choiceStr.equals("3")) {
-                            cnpListVoted.add(cnpReader);
-                            cnpListNonvoted.remove(cnpReader);
-                            costache.setVotes(costache.getVotes().add(BigDecimal.valueOf(1)));
-                            counterVote = counterVote + 1;
-                            System.out.println("So far " + counterVote + " people voted");
+                                popescu.setVotes(popescu.getVotes().add(BigDecimal.valueOf(1)));
+                                counterVote = counterVote + 1;
+                                System.out.println("So far " + counterVote + " people voted");
 
-                        } else if (choiceStr.equals("4")) {
-                            cnpListVoted.add(cnpReader);
-                            cnpListNonvoted.remove(cnpReader);
-                            voicu.setVotes(voicu.getVotes().add(BigDecimal.valueOf(1)));
-                            counterVote = counterVote + 1;
-                            System.out.println("So far " + counterVote + " people voted");
+                            } else if (choiceStr.equals("3")) {
+                                cnpListVoted.add(cnpReader);
+                                costache.setVotes(costache.getVotes().add(BigDecimal.valueOf(1)));
+                                counterVote = counterVote + 1;
+                                System.out.println("So far " + counterVote + " people voted");
 
-                        } else if (choiceStr.equals("5")) {
-                            cnpListVoted.add(cnpReader);
-                            cnpListNonvoted.remove(cnpReader);
-                            canceledVotes.setVotes(canceledVotes.getVotes().add(BigDecimal.valueOf(1)));
-                            counterVote = counterVote + 1;
-                            System.out.println("So far " + counterVote + " people voted");
+                            } else if (choiceStr.equals("4")) {
+                                cnpListVoted.add(cnpReader);
+                                voicu.setVotes(voicu.getVotes().add(BigDecimal.valueOf(1)));
+                                counterVote = counterVote + 1;
+                                System.out.println("So far " + counterVote + " people voted");
 
+                            } else if (choiceStr.equals("5")) {
+                                cnpListVoted.add(cnpReader);
+                                canceledVotes.setVotes(canceledVotes.getVotes().add(BigDecimal.valueOf(1)));
+                                counterVote = counterVote + 1;
+                                System.out.println("So far " + counterVote + " people voted");
+
+                            } else {
+                                System.out.println("Your choice doen't match the option");
+                                System.out.println("Do you want to continue elections, please type \"yes\" if you do,\n" +
+                                        "or type anything else in order to finish the elections ");
+                                strAnswer = input.nextLine();
+                            }
                         } else {
-                            System.out.println("Your choice doen't match the option");
-                            System.out.println("Do you want to continue elections, please type \"yes\" if you do,\n" +
-                                    "or type anything else in order to finish the elections ");
-                            strAnswer = input.nextLine();
+                            System.out.println("You already voted, you can't vote again ");
                         }
+                    } else {
+                        System.out.println("You are not on the list therefor you can't vote in this polling place");
                     }
+                } else if (actionStr.equals("2")) {
+                    System.out.println("The curent situation is");
+                    checkStatus(candidats, canceledVotes, counterVote, checkElectionFinish);
+                } else if (actionStr.equals("3") && counterVote > 0) {
+                    checkElectionFinish = 1;
+                    System.out.println("The elections are finished\n" +
+                            "The final results are");
+                    checkStatus(candidats, canceledVotes, counterVote, checkElectionFinish);
+                    strAnswer = "No";
+
+                } else if (actionStr.equals("3") && counterVote == 0) {
+                    System.out.println("Election were abandoned as no vote was expressed.");
+                    strAnswer = "No";
                 }
-            } else if (actionStr.equals("2")) {
-                checkStatus(candidats);
+            }//aici
+            else if (counterVote == 10) {
+                checkElectionFinish = 1;
+                System.out.println("The elections are finished\n" +
+                        "The final results are");
+                checkStatus(candidats, canceledVotes, counterVote, checkElectionFinish);
+                break;
             }
 
         }
     }
 
-    public static void checkStatus(Set<Candidat> candidats) {
-        Comparator<Candidat> comparatorulMeu = new Comparator<Candidat>() {
-            //Comparator<Candidat> comparatorulMeu = (o1, o2) ->
-            @Override
-            public int compare(Candidat o1, Candidat o2) {
-                int i = 0;
-                if (Integer.parseInt(o1.getVotes().toString()) > Integer.parseInt(o2.getVotes().toString())) {
-                    i = -1;
-                } else if (Integer.parseInt(o1.getVotes().toString()) < Integer.parseInt(o2.getVotes().toString())) {
-                    i = 1;
-                } else {
-                    i = o1.getName().compareTo(o2.getName());
+
+    public static void checkStatus(Set<Candidat> candidats, Candidat canceledVotes, int votes, int checkelectionsFinish) {
+        if (votes > 0) {
+            Comparator<Candidat> comparatorulMeu = new Comparator<Candidat>() {
+                //Comparator<Candidat> comparatorulMeu = (o1, o2) ->
+                @Override
+                public int compare(Candidat o1, Candidat o2) {
+                    int i = 0;
+                    if (Integer.parseInt(o1.getVotes().toString()) > Integer.parseInt(o2.getVotes().toString())) {
+                        i = -1;
+                    } else if (Integer.parseInt(o1.getVotes().toString()) < Integer.parseInt(o2.getVotes().toString())) {
+                        i = 1;
+                    } else {
+                        i = o1.getName().compareTo(o2.getName());
+                    }
+                    return i;
                 }
-                return i;
+            };
+            int position = 1;
+            Set<Candidat> candidatArrList1 = new TreeSet<>(comparatorulMeu);
+            candidatArrList1.addAll(candidats);
+            List<Candidat> candidatArrList = new ArrayList<>();
+            candidatArrList.addAll(candidatArrList1);
+            List<Candidat> candidats1 = new ArrayList<>();
+            candidats1.add(candidatArrList.get(0));
+            for (int i = 1; i <= candidatArrList.size() - 1; i++) {
+                if (candidatArrList.get(i).getVotes().equals(candidatArrList.get(0).getVotes())) {
+                    candidats1.add(candidatArrList.get(i));
+                }
             }
-        };
-        Set<Candidat> candidatArrList1 = new TreeSet<>(comparatorulMeu);
-        candidatArrList1.addAll(candidats);
-        List<Candidat> candidatArrList = new ArrayList<>();
-        candidatArrList.addAll(candidatArrList1);
 
-        for (int i = 0; i <= candidatArrList.size() - 1; i++) {
 
-            System.out.println("on position " + (i + 1) + " we have the candidate " + candidatArrList.get(i).getName() + " which has " + candidatArrList.get(i).getVotes() + " votes");
+            System.out.println("on position 1 we have the candidate " + candidatArrList.get(0).getName() + " which has " + candidatArrList.get(0).getVotes() + " vote(s) ");
+
+
+            for (int i = 1; i <= candidatArrList.size() - 1; i++) {
+                if (!candidatArrList.get(i - 1).getVotes().equals(candidatArrList.get(i).getVotes())) {
+                    position = i + 1;
+
+
+                    System.out.println("on position " + (i + 1) + " we have the candidate " + candidatArrList.get(i).getName() + " which has " + candidatArrList.get(i).getVotes() + " vote(s) "
+                    );
+                } else {
+                    System.out.println("on position " + (position) + " we have the candidate " + candidatArrList.get(i).getName() + " which has " + candidatArrList.get(i).getVotes() + " vote(s)"
+                    );
+                }
+
+            }
+            if (!canceledVotes.getVotes().equals(0)) {
+                System.out.println("And " + canceledVotes.getVotes() + " vote(s) were invalidated");
+
+            }
+            if (checkelectionsFinish == 1) {
+                if (candidats1.size() == 1) {
+                    System.out.println("WE HAVE A WINNER\n" +
+                            "Mr. " + candidats1.get(0).getName() + " won the elections");
+                } else {
+                    System.out.println("WE HAVE A TIE");
+                    for (int j = 0; j <= candidats1.size() - 1; j++) {
+                        System.out.println("Mr. " + candidats1.get(j).getName());
+
+                    }
+                    System.out.println("Will go to he second round");
+                }
+            }
+
+            //candidats.forEach(candidat -> System.out.println("suntem aici" + candidat));
+        } else {
+            System.out.println("No vote was counted, there is no intermediate pool available");
         }
-        //candidats.forEach(candidat -> System.out.println("suntem aici" + candidat));
 
 
     }
 
 }
+
+// (candidatArrList.get(i).getVotes().divide(BigDecimal.valueOf(votes).subtract(canceledVotes.getVotes()))).multiply(BigDecimal.valueOf(100)) + " %, of available votes")
+// +
+//                    ((Float.parseFloat(candidatArrList.get(0).getVotes().toString())* Float.parseFloat(BigDecimal.valueOf(votes).subtract(canceledVotes.getVotes()).toString()))*10) + " %, of available votes"
 
